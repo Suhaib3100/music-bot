500dad1a9500c0eb840ba8d3016ed2d3b6298fd4
Suhaib King <109773912+Suhaib3100@users.noreply.github.com>
2024-06-08 13:52:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 require('dotenv').config();
 module.exports = {
     SUPPORT_SERVER: 'https://discord.gg/darkcityrp',
-    OWNER_ID: "",// Your discord id
-    CLIENT_ID: '',// Your Bot id
+    OWNER_ID: "800265791043534848",// Your discord id
+    CLIENT_ID: '1247209531084374098',// Your Bot id
     PREFIX: '!',// Your general prefix
     PERMISSIONS: '8',
     BOT_PRESENCE: {
         ENABLED: true, // Whether or not the bot should update its status
-        STATUS: "idle", // The bot's status [online, idle, dnd, invisible]
-        TYPE: 2, // Status type for the bot [PLAYING = 0 | STREAMING = 1 | LISTENING = 2 | WATCHING = 3 | CUSTOM = 4 | COMPETING = 5 ]
-        MESSAGE: "/play | On {servers} Guilds", // Your bot status message, {users} = users size on all the servers | {servers} = servers size that are available
+        STATUS: "dnd", // The bot's status [online, idle, dnd, invisible]
+        TYPE: 1, // Status type for the bot [PLAYING = 0 | STREAMING = 1 | LISTENING = 2 | WATCHING = 3 | CUSTOM = 4 | COMPETING = 5 ]
+        MESSAGE: "at Kings Music", // Your bot status message, {users} = users size on all the servers | {servers} = servers size that are available
     },
     DASHBOARD: {
         ENABLED: true, // true, if you want the dashboard to be shown, false otherwise [disabled]
         CLIENT_ID: process.env.CLIENT_ID || "", // your bot client ID
-        CLIENT_SECRET: process.env.CLIENT_SECRET || "", // your bot client secret
+        CLIENT_SECRET: process.env.CLIENT_SECRET || "", // your bot client secreti
         PASSWORD: process.env.TOP_SECRET_PASSWORD || "mypassword", // your top secret password, you can change it to anything you want
         PORT: process.env.PORT || 80, // your dashboard port number
-        dashboardURL: "http://localhost",
+        dashboardURL: "https://c21c15b8-63bf-44f9-9a5e-22c2275f551d-00-3l5fcqwbsatnd.sisko.replit.dev", // change it to your dashboard url
         callbackURL: "/auth", //don't change this
     },
     MUSIC: {
         IDLE_TIME: 10,//time in seconds before the client disconnects from the voice channel.
-        A24: true, // true = continue playing 24/7H, false = stop playing when no one listens to the bot
+        A24: false, // true = continue playing 24/7H, false = stop playing when no one listens to the bot
     },
     NODES: [  
         {
-            host: "lava.link",
+            host: "node.raidenbot.xyz",
             identifier: 'main1',
-            port: 80,
-            password: "anypassword",
+            port: 5500,
+            password: "pwd",
             secure:  false,
             retryAmount: 30,
             retryDelay: 10000
